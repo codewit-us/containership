fc4e4cdb26c89a0351fb64168e0c7fd64d4a3030
Kevin Buffardi <kbuffardi@csuchico.edu>
2025-06-10 17:29:58 -0700

    Add C++ example with i/o capture and submission enclosed in code namespace

---
+++ cpp/code.h
@@ -0,0 +1,18 @@
+/* prefix block start */
+namespace code{
+/* prefix block end */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  string input;
+  cin>>input;
+  cout<<"Hi, "<<input<<endl;
+
+  return 0;
+}
+/* postfix block start*/
+}//end namespace
+/* postfix block end*/
--- cpp/codewit_test.h
+++ cpp/codewit_test.h
@@ -1,10 +1,46 @@
 #include <cxxtest/TestSuite.h>
+#include <sstream>
+#include <iostream>
+#include "code.h"
+
           
 class codewit_test : public CxxTest::TestSuite 
 {
    public:
-      void testAddition( void ){
-         TS_ASSERT( 1 + 1 > 1 );
-         TS_ASSERT_EQUALS( 1 + 1, 2 );
+      void testJuan(){
+        std::stringstream in("juan");
+        std::streambuf *cin_buf = std::cin.rdbuf(); //save old buf
+        std::cin.rdbuf(in.rdbuf()); //redirect std::cin to in input stream
+        
+        std::stringstream out;
+        std::streambuf *cout_buf = std::cout.rdbuf(); //save old buf
+        std::cout.rdbuf(out.rdbuf()); //redirect std::cout to out.txt!
+
+        //invoke submitted code (as nested in submission class)
+        //code::submission runner;
+        code::main();
+
+        std::cin.rdbuf(cin_buf);   //reset to standard input again
+        std::cout.rdbuf(cout_buf); //reset to standard output again
+
+        TS_ASSERT_EQUALS( "Hi, juan\n", out.str() );
+      }
+      void testDestiny(){
+        std::stringstream in("Destiny");
+        std::streambuf *cin_buf = std::cin.rdbuf(); //save old buf
+        std::cin.rdbuf(in.rdbuf()); //redirect std::cin to in input stream
+        
+        std::stringstream out;
+        std::streambuf *cout_buf = std::cout.rdbuf(); //save old buf
+        std::cout.rdbuf(out.rdbuf()); //redirect std::cout to out.txt!
+
+        //invoke submitted code (as nested in submission class)
+        //code::submission runner;
+        code::main();
+
+        std::cin.rdbuf(cin_buf);   //reset to standard input again
+        std::cout.rdbuf(cout_buf); //reset to standard output again
+
+        TS_ASSERT_EQUALS( "Hi, Destiny\n", out.str() );
       }
 };
